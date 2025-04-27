95b2782da1b5d96090ddeef09b8583496e56a7f8
bamboo_shoot <156806318+kon65138@users.noreply.github.com>
2025-04-27 17:04:06 +0100

    make header look better

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,12 @@
+import githubImg from '../assets/github-original.svg'
 export function Header(){
     return (
         <header>
-            <h1>cv generator</h1>
-            <div>by <a href="https://github.com/kon65138">kon65138</a></div>
+            <h1 className="title">cv generator</h1>
+            <div>by kon65138</div>
+            <a className="githubLink" href='https://github.com/kon65138' target='_blank'>
+                    <img src={githubImg} alt="github logo" />
+            </a>
         </header>
     )
 }
